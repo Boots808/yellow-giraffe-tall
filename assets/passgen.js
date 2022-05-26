9e54d93be4eacb91d69e8d27ae3440d3f923da10
brs <brv1114@yahoo.com>
2022-05-25 20:19:52 -0500

    project code completion

--- assets/passgen.js
+++ assets/passgen.js
@@ -1,5 +1,16 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate.Btn");
+var myInput = document.getElementById("password");
+
+const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*_-+=";
+
+const passwordTxt = document.getElementById("password");
+const length = document.getElementById("length");
+const incNumbers = document.getElementById("numbers");
+const incSymbols = document.getElementById("symbols");
+const generateBtn = document.getElementById("generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -8,7 +19,73 @@ function writePassword() {
 
   passwordText.value = password;
 
+  generateBtn.addEventListener("click", () => {
+    let characters = alpha;
+    incNumbers.checked ? (characters += numbers) : "";
+    incSymbols.checked ? (characters += symbols) : "";
+    passwordTxt.value = generatePassword(length.value, characters);
+  });
+
+  const generatePassword = (length, characters) => {
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      password += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+    return password;
+  };
+
 }
+
+myInput.onfocus = function() {
+  documentgetElementById("password").style.display = "block";
+}
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("password").style.display = "block";
+}
+
+//Prompts asking for password length
+function chooseLength() {
+  userInfo.passLength = prompt("Enter a number between 8 and 128");
+  }
+//Ensure length is within parameters
+if(myInput.value.length >= 8) {
+  length.classList.remove("invalid");
+  length.classList.add("valid");
+} else {
+  length.classList.remove("valid");
+  length.classList.add("invalid");
+    }
+  }
+}
+
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+//THEN I am presented with a series of prompts for password criteria
+//WHEN prompted for password criteria
+//THEN I select which criteria to include in the password
+//WHEN prompted for the length of the password
+//THEN I choose a length of at least 8 characters and no more than 128 characters
+//WHEN asked for character types to include in the password
+//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+//WHEN I answer each prompt
+//THEN my input should be validated and at least one character type should be selected
+//WHEN all prompts are answered
+//THEN a password is generated that matches the selected criteria
+//WHEN the password is generated
+//THEN the password is either displayed in an alert or written to the page
