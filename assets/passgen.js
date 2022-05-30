fa8aa1f580bbeea9e47dda6ab60258821bd3e67c
brs <brv1114@yahoo.com>
2022-05-29 22:28:19 -0500

    updates to code for regrading

--- assets/passgen.js
+++ assets/passgen.js
@@ -1,91 +1,69 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate.Btn");
-var myInput = document.getElementById("password");
 
-const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numbers = "0123456789";
-const symbols = "!@#$%^&*_-+=";
+// Assignment Code here
+var userInfo = {
+  passLength: 0,
+  chars: "",
+  password: ""
+};
 
-const passwordTxt = document.getElementById("password");
-const length = document.getElementById("length");
-const incNumbers = document.getElementById("numbers");
-const incSymbols = document.getElementById("symbols");
-const generateBtn = document.getElementById("generate");
+// characters to use
+var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericChars = "0123456789";
+var specialChars = /[!"#$%&'()*+,-./:;<=>?@[\]^_{|}~`]/;
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-  generateBtn.addEventListener("click", () => {
-    let characters = alpha;
-    incNumbers.checked ? (characters += numbers) : "";
-    incSymbols.checked ? (characters += symbols) : "";
-    passwordTxt.value = generatePassword(length.value, characters);
-  });
-
-  const generatePassword = (length, characters) => {
-    let password = "";
-    for (let i = 0; i < length; i++) {
-      password += characters.charAt(
-        Math.floor(Math.random() * characters.length)
-      );
-    }
-    return password;
-  };
-
-}
-
-myInput.onfocus = function() {
-  documentgetElementById("password").style.display = "block";
-}
-
-// When the user clicks on the password field, show the message box
-myInput.onfocus = function() {
-  document.getElementById("password").style.display = "block";
-}
 
 //Prompts asking for password length
 function chooseLength() {
   userInfo.passLength = prompt("Enter a number between 8 and 128");
-  }
-//Ensure length is within parameters
-if(myInput.value.length >= 8) {
-  length.classList.remove("invalid");
-  length.classList.add("valid");
-} else {
-  length.classList.remove("valid");
-  length.classList.add("invalid");
-    }
-  }
+  chooseLength();
 }
 
+  // confirm length parameters
+if(userInfo.value.length >= 8) {
+  userInfo.passLength.remove("invalid");
+  userInfo.passLength.add("valid");
+} else {
+  userInfo.passLength.remove("valid");
+  userInfo.passLength.add("invalid");
+    }
 
+//user to define password criteria
+function chooseChars() {
+  userInfo.chars="";
+  var numericCheck = confirm("Include numbers in your password?");
+  var specialCheck = confirm("Include special characters?");
+  var upperCaseCheck = confirm("Include Upper Case Letters?");
 
+  if (numericCheck) {
+    userInfo.chars += numericChars;
+  }
 
+  if (specialCheck) {
+    userInfo.chars += specialChars;
+  }
+
+  if (upperCaseCheck) {
+    userInfo.chars += uppercaseChars;
+  }
+};
+
+function generatePassword() {
+
+// Alert Message
+function alert ();
+  window.alert("Answer the following to receive your generated password");
+
+};
+
+// Write password to the #password input
+
+function writePassword() {
+  console.log('Password');
+  return userInfo.password;
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-
-
-
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-//THEN I am presented with a series of prompts for password criteria
-//WHEN prompted for password criteria
-//THEN I select which criteria to include in the password
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
-//WHEN asked for character types to include in the password
-//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
-//WHEN all prompts are answered
-//THEN a password is generated that matches the selected criteria
-//WHEN the password is generated
-//THEN the password is either displayed in an alert or written to the page
+}
