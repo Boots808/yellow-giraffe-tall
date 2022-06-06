81dab53e4744e74e01a424ce07ffb9147ab0eee3
brs <brv1114@yahoo.com>
2022-06-06 14:48:53 -0500

    restructuring of some of the javascript code in attempt to get working

--- assets/passgen.js
+++ assets/passgen.js
@@ -1,65 +1,52 @@
+//declare variables
+var passLength="";
+var password=document.getElementById("password");
+var specialChar;
+var lowerCaseChar;
+var upperCaseChar;
+var numericChar;
 
-// Assignment Code here
-var userInfo = {
-  passLength: 0,
-  chars: "",
-  password: "",
-};
+var numberArray = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27"];
+var lowerLetterArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperLetterArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var characterArray = ["!","@","#","$","%","^","&","*","(",")"];
 
 
-// characters to use
-var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numericChars = "0123456789";
-var specialChars = /[!"#$%&'()*+,-./:;<=>?@[\]^_{|}~`]/;
+//Prompt question when button is pushed
+document.querySelector("#generate").addEventListener("onclick", displayPassword);
 
-// Alert Message
 
-function prompt() {
-  window.alert("Let's get a password!");
-  window.alert("Use Lowercase Letters?");
-  window.alert("Use Uppercase Letters?");
-  window.alert("Use Special Characters?")
-}
-prompt();
-
-let password = Math.floor(math.random()*10);
-
-//Prompts asking for password length
-function passwordLength() {
-  window.alert(userInfo.passLength("Enter a number between 8 and 128"));
-  Length();
-}
-
-//user to define password criteria
-function chooseChars() {
-  userInfo.chars="";
-  var numericCheck = confirm("Include numbers in your password?");
-  var specialCheck = confirm("Include special characters?");
-  var upperCaseCheck = confirm("Include Upper Case Letters?");
-
-  if (numericCheck) {
-    userInfo.chars += numericChars;
+  function generatePassword() {
+    var passLength = (prompt("How many characters do you want to use for your password?"));
   }
 
-  if (specialCheck) {
-    userInfo.chars += specialChars;
+  //Alert if answer is not within specified range
+function genPassword() {
+  while (passLength <= 8 || passLength >= 128) {
+    alert("Password length MUST be between 8-128 characters!");
   }
 
-  if (upperCaseCheck) {
-    userInfo.chars += uppercaseChars;
-  }
-};
 
-// Write password to the #password input
-function generate() {
-  console.log(generate());
+//Password prompt questions using javascript confirm method
+var specialChar = confirm("Would you like to include special characters in your password?");
+var lowerCaseChar = confirm("Would you like to include lower case characters in your password?");
+var upperCaseChar = confirm("Would you like to include upper case characters in your password?");
+var numericChar = confirm("Would you like to include numbers in your password?");
+
+
+
+
+//password character arrays
 
 }
 
-// Add event listener to generate button
+//create loop for password generation
+for (var i = 0; <= passwordLength; i++) {
+  var randomNumber = Math.floor(Math.random() * chars.length);
+  password += chars.substring(randomNumber, randomNumber +1);
+}
 
-generateBtn.on('click', function () {
-  var Password = password(15);
-  passwordDisplay.text(Password);
-});
+
+
+//resources
+// https://dev.to/code_mystery/random-password-generator-using-javascript-6a
